f546b44250299a181853cd0f921d9eb7c1e4415b
Priyam <priyam009@live.com>
2019-10-23 20:30:20 +1100

    Add Header to page

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../App.css";
+
+function Header() {
+  return (
+    <div className="card header-content text-center">
+      <div className="card-body d-flex flex-column align-item-center">
+        <h1 className="card-title">Clicky Game!</h1>
+        <p className="card-text header-para">
+          Click on the image to earn points, but don't click on any more than
+          once!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
