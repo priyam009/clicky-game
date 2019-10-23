1e02648a24360cfc9517bb9adc8fc29c91d83321
Priyam <priyam009@live.com>
2019-10-23 20:29:58 +1100

    Add Navbar to page

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../App.css";
+
+function Navbar(props) {
+  return (
+    <nav className="navbar navbar-light d-flex flex-row justify-content-between nav-content">
+      <div className="d-flex">
+        <a href="/" className="navbar-brand">Clicky Game!</a>
+      </div>
+      <div>Click on the image to begin!</div>
+      <ul className="navbar-nav d-flex flex-row">
+        <li className="nav-item p-3">Score: {props.score}</li>
+        <li className="nav-item p-3">Top Score: {props.topScore}</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
