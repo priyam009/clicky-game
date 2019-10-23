6c2bc2800acb4de0afba184d1122aaa6243f63d8
Priyam <priyam009@live.com>
2019-10-23 20:27:50 +1100

    Update App.js with Navbar and Header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Game from "./components/Game";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    score: 0,
+    topScore: 0
+  };
+
+  render() {
+    return (
+      <div className="wrapper">
+        <Navbar score={this.state.score} topScore={this.state.topScore} />
+        <Header />
+        <Game />
+      </div>
+    );
+  }
 }
 
 export default App;
