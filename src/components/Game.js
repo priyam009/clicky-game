f8329b5ff7ccc6284c5658ea753a70907d264cd0
Priyam <priyam009@live.com>
2019-10-24 00:05:08 +1100

    Add Game function

---
+++ src/components/Game.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../App.css";
+
+function Game(props) {
+  // console.log(props.id);
+  return (
+    <button key={props.id} onClick={() => props.handleClick(props.id)}>
+      <div className="card character-card">
+        <div className="img-container">
+          <img alt={props.name} src={props.image} />
+        </div>
+      </div>
+    </button>
+  );
+}
+
+export default Game;
