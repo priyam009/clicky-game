c2a2d10a49f4a3fcfb185f57541a8779b72e495d
Priyam <priyam009@live.com>
2019-10-24 00:03:13 +1100

    Add Wrapper to control each character card, functionality to handle click on each card and update score

--- src/App.js
+++ src/App.js
@@ -2,21 +2,61 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
 import Game from "./components/Game";
+import characters from "./characters.json";
 
 class App extends React.Component {
   state = {
     score: 0,
-    topScore: 0
+    topScore: 0,
+    characters: characters
+  };
+
+  //Reset all character to "clicked = false"
+  resetCharacters = () => {
+    this.state.characters.forEach(character => character.clicked = false)
+  }
+
+  //Handle click on characters- update score
+  handleClick = id => {
+    const newCharacters = this.state.characters.filter(character => {
+      if (character.id === id && !character.clicked) {
+        character.clicked = true;
+        this.setState({
+          score: this.state.score + 1
+        })
+      } else if(character.id === id && character.clicked) {
+        this.resetCharacters();
+        this.setState({
+          score: 0
+        })
+      }
+      return character;
+    });
+
+    console.log("newCharacters", newCharacters);
+    this.setState({
+      characters: newCharacters
+    })
   };
 
   render() {
     return (
-      <div className="wrapper">
+      <>
         <Navbar score={this.state.score} topScore={this.state.topScore} />
         <Header />
-        <Game />
-      </div>
+        <Wrapper>
+          {this.state.characters.map(character => (
+            <Game
+              id={character.id}
+              name={character.name}
+              image={character.image}
+              handleClick={this.handleClick}
+            />
+          ))}
+        </Wrapper>
+      </>
     );
   }
 }
