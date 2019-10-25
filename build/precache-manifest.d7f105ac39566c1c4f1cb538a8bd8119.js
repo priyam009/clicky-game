13e58ac27ee9e66447625c248f2dd2f470f9549c
Priyam <priyam009@live.com>
2019-10-26 10:39:45 +1100

    Udpate build

---
+++ build/precache-manifest.d7f105ac39566c1c4f1cb538a8bd8119.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "398f0bfca1e0840be03e0bdab09f5f27",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "503b18a80407c76cfba4",
+    "url": "/clicky-game/static/css/main.f98e7d73.chunk.css"
+  },
+  {
+    "revision": "8559de696f673634bbf4",
+    "url": "/clicky-game/static/js/2.e12bc9b2.chunk.js"
+  },
+  {
+    "revision": "503b18a80407c76cfba4",
+    "url": "/clicky-game/static/js/main.eae51618.chunk.js"
+  },
+  {
+    "revision": "0f0a7c239744c2652e8a",
+    "url": "/clicky-game/static/js/runtime-main.1df8f772.js"
+  }
+]);
