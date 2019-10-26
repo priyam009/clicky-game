643ab656179bedd36f67901d488355708fd84ca3
Priyam <priyam009@live.com>
2019-10-26 16:08:57 +1100

    Update header text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,20 @@ import React from "react";
 import "../App.css";
 
 function Navbar(props) {
-  if(props.instructions==="You guessed correctly!") {
-    var classname = "nav-instruction correct"
-  } else if (props.instructions==="You guessed incorrectly!") {
-    var classname = "nav-instruction incorrect"
+  var classname
+  if(props.instructions === "Correct Guess") {
+    classname = "nav-instruction correct"
+  } else if (props.instructions === "Incorrect Guess") {
+    classname = "nav-instruction incorrect"
   } else {
-    var classname = "nav-instruction"
+    classname = "nav-instruction"
   }
 
   return (
     <nav className="navbar navbar-light d-flex flex-row justify-content-between nav-content">
       <div className="d-flex">
         <a href="/" className="navbar-brand">
-          Clicky Game!
+          CARTOON CLICK!
         </a>
       </div>
       <div className={classname}>{props.instructions}</div>
