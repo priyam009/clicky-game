7cf8528ee285004dfdc346d566b587c9f73cda1b
Priyam <priyam009@live.com>
2019-10-26 11:32:21 +1100

    Add class name to instruction

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,14 @@ import React from "react";
 import "../App.css";
 
 function Navbar(props) {
+  if(props.instructions==="You guessed correctly!") {
+    var classname = "nav-instruction correct"
+  } else if (props.instructions==="You guessed incorrectly!") {
+    var classname = "nav-instruction incorrect"
+  } else {
+    var classname = "nav-instruction"
+  }
+
   return (
     <nav className="navbar navbar-light d-flex flex-row justify-content-between nav-content">
       <div className="d-flex">
@@ -9,7 +17,7 @@ function Navbar(props) {
           Clicky Game!
         </a>
       </div>
-      <div className="nav-instruction">Click on the image to begin!</div>
+      <div className={classname}>{props.instructions}</div>
       <ul className="navbar-nav d-flex flex-row">
         <li className="nav-item p-3">Score: {props.score}</li>
         <li className="nav-item p-3">Top Score: {props.topScore}</li>
