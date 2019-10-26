9aa3ab27a742419d89fa0c9d0ecb2cd5a84dba3c
Priyam <priyam009@live.com>
2019-10-26 16:08:27 +1100

    Update app name

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="card header-content text-center">
       <div className="card-body d-flex flex-column align-item-center">
-        <h1 className="card-title">Clicky Game!</h1>
+        <h1 className="card-title">CLICK It to MAKE It!</h1>
         <p className="card-text header-para">
           Click on the image to earn points, but don't click on any more than
           once!
